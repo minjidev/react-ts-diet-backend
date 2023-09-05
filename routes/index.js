4c8d3f54bd62d466d9552fe6c2d7d65c80c04918
minji99710 <alswl99710@gmail.com>
2023-09-06 05:47:15 +0900

    FIx: Remove unnecessary route

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,9 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('./auth');
-const recipes = require('./recipes');
 const users = require('./users');
 
 router.use('/auth', auth);
-router.use('/recipes', recipes);
 router.use('/user', users);
 
 module.exports = router;
